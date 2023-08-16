5fb693f1e21f84e2b7cf7203f4008d33e6bfc76b
EmircanAydintug <emir2115@outlook.com>
2023-08-16 17:01:47 +0300

    SeriPort klasöründe SeriPort-PortlarArasiMesajlasma uygulamasında mesajlaşmayı sağlayacak kodlar ve arayüz yapıldı.

---
+++ SeriPort/SeriPort-PortlarArasiMesajlasma/SeriPort-PortlarArasiMesajlasma/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SeriPort_PortlarArasiMesajlasma
+{
+    partial class btnGonder
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxCOMsec = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.richTxtRecived = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBoxCOMsec
+            // 
+            this.comboBoxCOMsec.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBoxCOMsec.FormattingEnabled = true;
+            this.comboBoxCOMsec.Location = new System.Drawing.Point(48, 48);
+            this.comboBoxCOMsec.Name = "comboBoxCOMsec";
+            this.comboBoxCOMsec.Size = new System.Drawing.Size(166, 32);
+            this.comboBoxCOMsec.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(234, 28);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(82, 59);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Bağlan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // richTxtRecived
+            // 
+            this.richTxtRecived.Location = new System.Drawing.Point(48, 101);
+            this.richTxtRecived.Name = "richTxtRecived";
+            this.richTxtRecived.Size = new System.Drawing.Size(367, 216);
+            this.richTxtRecived.TabIndex = 2;
+            this.richTxtRecived.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMessage.Location = new System.Drawing.Point(48, 344);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(268, 29);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(333, 28);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(82, 59);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = "Bağlantıyı Kes";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(333, 323);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(82, 59);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Gönder";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnGonder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 436);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.richTxtRecived);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.comboBoxCOMsec);
+            this.Name = "btnGonder";
+            this.Text = "SeriPort-Mesajlaşma";
+            this.Load += new System.EventHandler(this.btnGonder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxCOMsec;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox richTxtRecived;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SeriPort/SeriPort-PortlarArasiMesajlasma/SeriPort-PortlarArasiMesajlasma/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeriPort_PortlarArasiMesajlasma
+{
+    public partial class btnGonder : Form
+    {
+        public btnGonder()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGonder_Load(object sender, EventArgs e)
+        {
+            
+
+            foreach (var seriPort in SerialPort.GetPortNames())
+            {
+                comboBoxCOMsec.Items.Add(seriPort);
+            }
+
+            
+            btnDisconnect.Enabled = false;
+            comboBoxCOMsec.SelectedIndex = 0;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxCOMsec.Text;
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = Parity.Even;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Seri Port bağlantısı yapılamadı \n Hata :{ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                btnSend.Enabled = true;
+            }
+
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnSend.Enabled = false;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+
+                richTxtRecived.Text += Environment.NewLine + txtMessage;
+                serialPort1.Write(txtMessage.Text);
+                txtMessage.Clear();
+                
+            }
+        }
+
+        public delegate void veriGoster(String s);
+        
+        public void txtYaz(String s)
+        {
+            richTxtRecived.Text += Environment.NewLine + s;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string gelenVeri = serialPort1.ReadExisting();
+            richTxtRecived.Invoke(new veriGoster(txtYaz), gelenVeri);
+        }
+    }
+}
